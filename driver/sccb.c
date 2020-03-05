a6e2ac208e29051828f531eb9a8514fa6e236b1c
apiesse <mirco.pattera@gmail.com>
2020-03-05 17:30:21 +0100

    Hardware I2C0/I2C1 for sccb communication

--- driver/sccb.c
+++ driver/sccb.c
@@ -19,6 +19,13 @@
 static const char* TAG = "sccb";
 #endif
 
+#ifdef CONFIG_SCCB_HARDWARE_I2C0
+ #define CONFIG_SCCB_HARDWARE_I2C
+#endif
+#ifdef CONFIG_SCCB_HARDWARE_I2C1
+ #define CONFIG_SCCB_HARDWARE_I2C
+#endif
+
 #define LITTLETOBIG(x)          ((x<<8)|(x>>8))
 
 #ifdef CONFIG_SCCB_HARDWARE_I2C
@@ -31,7 +38,11 @@ static const char* TAG = "sccb";
 #define ACK_CHECK_DIS           0x0              /*!< I2C master will not check ack from slave */
 #define ACK_VAL                 0x0              /*!< I2C ack value */
 #define NACK_VAL                0x1              /*!< I2C nack value */
+#if CONFIG_SCCB_HARDWARE_I2C1
 const int SCCB_I2C_PORT         = 1;
+#else
+const int SCCB_I2C_PORT         = 0;
+#endif
 static uint8_t ESP_SLAVE_ADDR   = 0x3c;
 #else
 #include "twi.h"
