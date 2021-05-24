7933e48dc8954d10157943979001a76ddc238a66
zhouli <zhouli@espressif.com>
2021-05-24 20:53:54 +0800

    fix_tjpgd_configuration for esp32s2

--- target/esp32s2/private_include/tjpgd.h
+++ target/esp32s2/private_include/tjpgd.h
@@ -7,7 +7,7 @@
 /* System Configurations */
 
 #define	JD_SZBUF		512	/* Size of stream input buffer */
-#define JD_FORMAT		1	/* Output pixel format 0:RGB888 (3 BYTE/pix), 1:RGB565 (1 WORD/pix) */
+#define JD_FORMAT		0	/* Output pixel format 0:RGB888 (3 BYTE/pix), 1:RGB565 (1 WORD/pix) */
 #define	JD_USE_SCALE	1	/* Use descaling feature for output */
 #define JD_TBLCLIP		1	/* Use table for saturation (might be a bit faster but increases 1K bytes of code size) */
 
