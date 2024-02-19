84f446b45acd0d578f8b363f5502bd6773312a35
Andy Blight <a.j.blight@leeds.ac.uk>
2024-02-19 13:50:00 +0000

    Fixed platformio/arduino build.
    The build fails with this error:
    
    Building in release mode
    Compiling .pio/build/esp32cam/.pio/libdeps/esp32cam/esp32-camera/target/xclk.c.o
    In file included from .pio/libdeps/esp32cam/esp32-camera/target/xclk.c:10:
    .pio/libdeps/esp32cam/esp32-camera/target/xclk.c: In function 'xclk_timer_conf':
    .pio/libdeps/esp32cam/esp32-camera/target/xclk.c:32:18: error: 'TAG' undeclared (first use in this function)
             ESP_LOGE(TAG, "ledc_timer_config failed for freq %d, rc=%x", xclk_freq_hz, err);
    
    This commit fixes the problem.

--- target/xclk.c
+++ target/xclk.c
@@ -10,8 +10,8 @@
 #include "esp32-hal-log.h"
 #else
 #include "esp_log.h"
-static const char* TAG = "camera_xclk";
 #endif
+static const char* TAG = "camera_xclk";
 
 #define NO_CAMERA_LEDC_CHANNEL 0xFF
 static ledc_channel_t g_ledc_channel = NO_CAMERA_LEDC_CHANNEL;
@@ -23,10 +23,10 @@ esp_err_t xclk_timer_conf(int ledc_timer, int xclk_freq_hz)
     timer_conf.freq_hz = xclk_freq_hz;
     timer_conf.speed_mode = LEDC_LOW_SPEED_MODE;
 
-#if ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(5, 2, 0)   
+#if ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(5, 2, 0)
     timer_conf.deconfigure = false;
 #endif
-   
+
 #if ESP_IDF_VERSION_MAJOR >= 4
     timer_conf.clk_cfg = LEDC_AUTO_CLK;
 #endif
