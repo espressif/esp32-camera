54355fe9c3ee1e3ecba9d0bc37504c1074e57168
Me No Dev <me-no-dev@users.noreply.github.com>
2024-03-21 13:42:57 +0200

    Update example to place frame buffer in PSRAM
    ESP32 failed to start the example

--- examples/camera_example/main/take_picture.c
+++ examples/camera_example/main/take_picture.c
@@ -125,6 +125,7 @@ static camera_config_t camera_config = {
 
     .jpeg_quality = 12, //0-63, for OV series camera sensors, lower number means higher quality
     .fb_count = 1,       //When jpeg mode is used, if fb_count more than one, the driver will work in continuous mode.
+    .fb_location = CAMERA_FB_IN_PSRAM,
     .grab_mode = CAMERA_GRAB_WHEN_EMPTY,
 };
 
